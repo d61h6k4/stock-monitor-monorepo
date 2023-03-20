454a23113656eeb1ab973d0a8ac712bfeae5a272
d61h6k4 <ddbihbka@gmail.com>
2023-03-20 23:07:48 +0100

    Create base tempalte

--- stock-monitor-backend/stock_monitor_backend/utils.py
+++ stock-monitor-backend/stock_monitor_backend/utils.py
@@ -4,7 +4,7 @@ from stock_monitor_backend.rules import Decision, Action
 def emojify(a: Action) -> str:
     match a:
         case Action.SELL:
-            return f"sell \U0001F4C9"
+            return f"**sell** \U0001F4C9"
         case Action.HOLD:
             return "hold"
         case Action.BUY:
@@ -12,8 +12,15 @@ def emojify(a: Action) -> str:
 
 
 def telegramify(decision: Decision) -> str:
-    description = decision.rule.description.replace('.', ',')
-    explanation = decision.explanation.replace('.', ',')
-    return (f"${decision.ticker} {emojify(decision.action)} \U000027A1 by {decision.rule.name}  "
-            f"{explanation}  "
-            f"{description}")
+    template = f"""
+    ${decision.ticker} {emojify(decision.action)} \U000027A1 by {decision.rule.name}
+
+
+    ##### {decision.explanation}
+
+
+    ###### {decision.rule.description}
+    """
+    for sym in ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '}', '{', '.', '!']:
+        template = template.repalce(sym, f"\\{sym}")
+    return template
