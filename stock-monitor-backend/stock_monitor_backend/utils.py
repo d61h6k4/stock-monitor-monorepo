b86262ce574094c9f9056cec088cd898c421a049
d61h6k4 <ddbihbka@gmail.com>
2023-03-20 20:15:02 +0100

    Fix unicode

--- stock-monitor-backend/stock_monitor_backend/utils.py
+++ stock-monitor-backend/stock_monitor_backend/utils.py
@@ -4,13 +4,13 @@ from stock_monitor_backend.rules import Decision, Action
 def emojify(a: Action) -> str:
     match a:
         case Action.SELL:
-            return f"sell \U+1F4C9"
+            return f"sell \\U+1F4C9"
         case Action.HOLD:
             return "hold"
         case Action.BUY:
-            return "buy \U+1F4C8"
+            return "buy \\U+1F4C8"
 
 
 def telegramify(decision: Decision) -> str:
-    return (f"Rule {decision.rule.name} \U+27A1 {emojify(decision.action)}.\n\n"
+    return (f"Rule {decision.rule.name} \\U+27A1 {emojify(decision.action)}.\n\n"
             f"<sup><sub>{decision.explanation}</sub></sup>")
