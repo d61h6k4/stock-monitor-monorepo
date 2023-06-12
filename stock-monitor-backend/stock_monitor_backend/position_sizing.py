75883655da7a62ae82231a81ec7831f187d702a6
d61h6k4 <ddbihbka@gmail.com>
2023-06-12 22:02:02 +0300

    Add forecast line

--- stock-monitor-backend/stock_monitor_backend/position_sizing.py
+++ stock-monitor-backend/stock_monitor_backend/position_sizing.py
@@ -3,26 +3,25 @@ import pandas as pd
 from datetime import datetime
 from datetime import timezone
 from pypfopt import black_litterman
-from pypfopt.efficient_frontier import EfficientFrontier
 from stock_monitor_data.data import portfolio
 from stock_monitor_data.models import Stock
 from typing import Mapping
 
 
 def get_market_prices() -> pd.Series:
-    return Stock(ticker_name="SPY", period="2y", interval="1d").history["Close"]
+    return Stock(ticker_name="SPY", period="5y", interval="1d").history["Close"]
 
 
 def get_portfolio_prices() -> pd.DataFrame:
     prices = []
-    for s in portfolio("2y", "1d"):
+    for s in portfolio("5y", "1d"):
         prices.append(s.history.rename(columns={"Close": s.ticker_name})[s.ticker_name])
     return pd.concat(prices, axis=1).fillna(0.0)
 
 
 def get_portfolio_market_cap() -> Mapping[str, float]:
     return {s.ticker_name: s.market_cap
-            for s in portfolio("2y", "1d")}
+            for s in portfolio("5y", "1d")}
 
 
 def get_portfolio_views() -> Mapping[str, float]:
@@ -34,11 +33,11 @@ def get_portfolio_views() -> Mapping[str, float]:
         return expected_excess / time_penalty
 
     return {s.ticker_name: _view(s)
-            for s in portfolio("2y", "1d")}
+            for s in portfolio("5y", "1d")}
 
 
 def get_portfolio_view_confidences() -> Mapping[str, float]:
-    confidences = pd.Series({s.ticker_name: s.expectation.confidence for s in portfolio("2y", "1d")})
+    confidences = pd.Series({s.ticker_name: s.expectation.confidence for s in portfolio("5y", "1d")})
     return confidences
 
 
--- stock-monitor-backend/stock_monitor_backend/wave/ideas.py
+++ stock-monitor-backend/stock_monitor_backend/wave/ideas.py
@@ -1,13 +1,61 @@
 from h2o_lightwave import Q, ui
 from stock_monitor_data.data import ideas
-from stock_monitor_backend.wave.common import stock_graph
+from stock_monitor_backend.wave.common import dataframe_to_data, preprocess_dataframe, color_stock_dataframe
 
 _CARD_REGISTER = []
 
 
+def idea_stock_graph(stock, box='1 3 11 5'):
+    y_min = stock.history["Low"].min() * 0.95
+    y_max = stock.history["High"].max() * 1.05
+    graph_data = dataframe_to_data(
+        color_stock_dataframe(
+            preprocess_dataframe(stock.history))[
+            ["Date", "Low", "High", "Open", "Close", "Color", "Volume"]])
+
+    return ui.form_card(
+        box=box,
+        items=[
+            ui.text_xl(stock.ticker_name),
+            ui.visualization(
+                name='stock',
+                height="260px",
+                data=graph_data,
+                plot=ui.plot(
+                    [ui.mark(type='schema',
+                             x_scale='time',
+                             x='=Date',
+                             y_q1="={{intl Open notation='compact' compactDisplay='short'}}",
+                             y_q2="={{intl Open notation='compact' compactDisplay='short'}}",
+                             y_q3="={{intl Close notation='compact' compactDisplay='short'}}",
+                             y1="={{intl Low notation='compact' compactDisplay='short'}}",
+                             y2="={{intl High notation='compact' compactDisplay='short'}}",
+                             y_min=y_min,
+                             y_max=max(y_max, stock.expectation.price * 1.05),
+                             color="=Color",
+                             color_range="#06982d #ae1325",
+                             color_domain=["green", "red"]),
+                     ui.mark(y=stock.expectation.price, label=f"Expected price {stock.expectation.price}", )])),
+            ui.visualization(
+                name='volume',
+                height="100px",
+                data=graph_data,
+                plot=ui.plot(
+                    [ui.mark(type='interval', x_scale='time', x='=Date',
+                             y_scale="quantile",
+                             y="={{intl Volume notation='compact' compactDisplay='short'}}",
+                             color="=Color",
+                             y_nice=True,
+                             color_range="#06982d #ae1325",
+                             color_domain=["green", "red"])
+                     ])),
+        ]
+    )
+
+
 async def render(q: Q):
     for ix, stock in enumerate(ideas(period="6mo", interval="1d")):
-        graph = stock_graph(stock, box=f"1 {2 + 6 * ix} 11 6")
+        graph = idea_stock_graph(stock, box=f"1 {2 + 6 * ix} 11 6")
         graph.items.append(ui.text_s(stock.description))
         q.page[f"stock_graph_{ix}"] = graph
         _CARD_REGISTER.append(f"stock_graph_{ix}")
