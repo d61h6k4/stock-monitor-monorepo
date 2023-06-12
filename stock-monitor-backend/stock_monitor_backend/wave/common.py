d861e0a32f9709db99007164a5d96251a8357783
d61h6k4 <ddbihbka@gmail.com>
2023-06-12 20:09:09 +0300

    Change report

--- stock-monitor-backend/stock_monitor_backend/wave/common.py
+++ stock-monitor-backend/stock_monitor_backend/wave/common.py
@@ -18,45 +18,47 @@ def dataframe_to_data(df):
     return data(df.columns.to_list(), df.shape[0], rows=df.values.tolist(), pack=True)
 
 
-def stock_graph(stock, box='1 3 11 5'):
+def stock_graph(stock, box='1 3 11 5', with_summary=False):
     graph_data = dataframe_to_data(
         color_stock_dataframe(
             preprocess_dataframe(stock.history))[
             ["Date", "Low", "High", "Open", "Close", "Color", "Volume"]])
+    items = [
+        ui.text_xl(stock.ticker_name),
+        ui.visualization(
+            name='stock',
+            height="260px",
+            data=graph_data,
+            plot=ui.plot(
+                [ui.mark(type='schema',
+                         x_scale='time',
+                         x='=Date',
+                         y_q1="={{intl Open notation='compact' compactDisplay='short'}}",
+                         y_q2="={{intl Open notation='compact' compactDisplay='short'}}",
+                         y_q3="={{intl Close notation='compact' compactDisplay='short'}}",
+                         y1="={{intl Low notation='compact' compactDisplay='short'}}",
+                         y2="={{intl High notation='compact' compactDisplay='short'}}",
+                         y_nice=True,
+                         color="=Color",
+                         color_range="#06982d #ae1325",
+                         color_domain=["green", "red"])])),
+        ui.visualization(
+            name='volume',
+            height="100px",
+            data=graph_data,
+            plot=ui.plot(
+                [ui.mark(type='interval', x_scale='time', x='=Date',
+                         y_scale="quantile",
+                         y="={{intl Volume notation='compact' compactDisplay='short'}}",
+                         color="=Color",
+                         y_nice=True,
+                         color_range="#06982d #ae1325",
+                         color_domain=["green", "red"])
+                 ])),
+    ]
+    if with_summary:
+        items.append(ui.text_s(stock.business_summary))
     return ui.form_card(
         box=box,
-        items=[
-            ui.text_xl(stock.ticker_name),
-            ui.visualization(
-                name='stock',
-                height="260px",
-                data=graph_data,
-                plot=ui.plot(
-                    [ui.mark(type='schema',
-                             x_scale='time',
-                             x='=Date',
-                             y_q1="={{intl Open notation='compact' compactDisplay='short'}}",
-                             y_q2="={{intl Open notation='compact' compactDisplay='short'}}",
-                             y_q3="={{intl Close notation='compact' compactDisplay='short'}}",
-                             y1="={{intl Low notation='compact' compactDisplay='short'}}",
-                             y2="={{intl High notation='compact' compactDisplay='short'}}",
-                             y_nice=True,
-                             color="=Color",
-                             color_range="#06982d #ae1325",
-                             color_domain=["green", "red"])])),
-            ui.visualization(
-                name='volume',
-                height="100px",
-                data=graph_data,
-                plot=ui.plot(
-                    [ui.mark(type='interval', x_scale='time', x='=Date',
-                             y_scale="quantile",
-                             y="={{intl Volume notation='compact' compactDisplay='short'}}",
-                             color="=Color",
-                             y_nice=True,
-                             color_range="#06982d #ae1325",
-                             color_domain=["green", "red"])
-                     ])),
-            ui.text_s(stock.business_summary),
-        ]
+        items=items
     )
