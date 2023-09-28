5dde27f80eb0459405d763ac0438d8502214e7ea
Danila <ddbihbka@gmail.com>
2023-09-28 11:00:03 +0200

    Fix link escaping

--- frontend/tests/test_utils.py
+++ frontend/tests/test_utils.py
@@ -3,6 +3,8 @@ from frontend.utils import escape_markdown
 
 def test_escape_markdown():
     assert (
-        escape_markdown("Google's last price is $120")
-        == "Google\\'s last price is \\$120"
+        escape_markdown(
+            "Google's last price is $120[Source](https://mcusercontent.com/)"
+        )
+        == "Google\\'s last price is \\$120[Source](https://mcusercontent.com/)"
     )
